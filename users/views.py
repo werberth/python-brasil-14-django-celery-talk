f266353beab92588ee087e516789023e47aa197b
werberth <werberthvinicius@gmail.com>
2018-10-25 01:21:18 -0300

    Call bulk_create_users_task celery task in bulk_create_users view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,14 @@
+import csv
 from django.shortcuts import render
+from .tasks import bulk_create_users_task
 
 def bulk_create_users(request):
-    return render(request, 'bulk-create-users.html')
+    context = {}
+    if request.method == 'POST':
+        csv_file = request.FILES.get('csv')
+        csv_file = csv_file.read().decode('utf-8').splitlines()
+        users = csv.DictReader(csv_file, delimiter=',')
+        bulk_create_users_task.delay(list(users))
+        succes_text = 'A sua solicitação esta sendo processada, você receberá um email assim quer terminarmos'
+        context['message'] = succes_text
+    return render(request, 'bulk-create-users.html', context)
