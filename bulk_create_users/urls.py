ba34e9ee48ab9cbec968f5030e2901e346fe0067
werberth <werberthvinicius@gmail.com>
2018-10-28 00:09:39 -0300

    Setup static files

--- bulk_create_users/urls.py
+++ bulk_create_users/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url, include
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 import users
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('users.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
